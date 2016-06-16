2bf1b3c5f573f2d24bc39c4dcc6a149c7d6339c4
Alec Snyder <linuxbash8@gmail.com>
2016-06-16 15:59:43 -0700

    add create user

--- example/testapp/apitest/src/db/db.go
+++ example/testapp/apitest/src/db/db.go
@@ -4,17 +4,31 @@ package db
 import (
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+  "user"
 )
 
 // openDB opens the given sqlite database by filepath.
-func openDB(filename string) (*sql.DB, error) {
+func OpenDB(filename string) (*sql.DB, error) {
 	return sql.Open("sqlite3", filename)
 }
 
+// CommitUser commits the given user to the given database
+func CommitUser(db *sql.DB, user user.User) error {
+  stmt, err := db.Prepare("insert into users values(NULL,?,?)")
+  if err != nil {
+    return err
+  }
+  defer stmt.Close()
+  _, err = stmt.Exec(user.Name, user.Age)
+  if err != nil {
+    return err
+  }
+  return nil
+}
 
 // CreateSkelDB creates a skeleton database with one user table and no users.
 func CreateSkelDB() error {
-	db, err := openDB("start.db")
+	db, err := OpenDB("start.db")
 	if err != nil {
 		return err
 	}
--- example/testapp/apitest/src/user/user.go
+++ example/testapp/apitest/src/user/user.go
@@ -8,3 +8,10 @@ type User struct {
 	Name    string
 	Age     int
 }
+
+func NewUser(name string, age int) User {
+	return User{
+		Name: name,
+		Age: age,
+	}
+}
