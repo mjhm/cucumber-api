927a6e79b2dfe5777ae2d2f7ae347e5a039df779
Alec Snyder <linuxbash8@gmail.com>
2016-06-20 11:20:42 -0700

    add wait for startup

--- example/testapp/apitest/features/support/feature_hooks.js
+++ example/testapp/apitest/features/support/feature_hooks.js
@@ -2,20 +2,38 @@
 var cucumberApi = require('cucumber-api');
 var Promise = require('bluebird');
 var childProcess = Promise.promisifyAll(require('child_process'), {multiArgs: true});
+var portUsed = require('tcp-port-used');
+
+waitForStartup = function (done) {
+  portUsed.check(8080, "localhost")
+    .then(function(inUse) {
+      if(inUse) {
+        done();
+      }
+      else {
+        setTimeout(function() {
+          waitForStartup(done);
+        }, 1000);
+      }
+    }, function (err) {
+      console.log("Cannot connect to server. Please make sure that your ports are configured properly");
+    });
+}
+
 
 module.exports = function () {
   var self = this;
 
   this.registerHandler('BeforeFeatures', function (event, done) {
-    childProcess.exec("../src/serv")
+    childProcess.exec("../src/serv");
     /* Start application and database here */
-    done()
+    waitForStartup(done);
   });
 
 
   this.registerHandler('AfterFeatures', function (event, done) {
     /* Stop application and db teardown here */
-    childProcess.exec("killall serv")
-    done()
+    childProcess.exec("killall serv");
+    done();
   });
 };
